594bd7d6f43d98f405ffa76b18f703bda8860cd7
unknown <Yuvraj>
2023-10-29 12:12:40 +0530

    Beta v1.1

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -2,9 +2,7 @@ import axios from "axios";
 
 let tempURL = "https://ask-wishdom-server.onrender.com/";
 // The axios Instance
-// 'https://ask-wishdom-server.onrender.com/'
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: tempURL,
 });
 
-// http://localhost:6969
